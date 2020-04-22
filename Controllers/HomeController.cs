753c6cfb70c7c6929ce5e5a63d75a834dd53fb04
ali <taheri669@gmail.com>
2020-04-22 09:22:51 +0430

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MailSender.Models;
+using MailSender.MailSender;
+
+namespace MailSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            EmailSender.SendEmail();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MailSender/Sender.cs
@@ -0,0 +1,51 @@
+﻿using Google.Apis.Admin.Directory.directory_v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MailSender.MailSender
+{
+    public class EmailSender 
+    {
+
+        public static string SendEmail()
+        {
+
+            try
+            {
+                // Credentials
+                var credentials = new NetworkCredential("user-name@gmail.com", "password");
+                // Mail message
+                var mail = new MailMessage()
+                {
+                    From = new MailAddress("user-name@gmail.com"), //sender
+                    Subject = "Email Sender App",
+                    Body = "It Is Test."
+                };
+                mail.IsBodyHtml = true;
+                mail.To.Add(new MailAddress("receiver@gmail.com"));
+                // Smtp client
+                var client = new SmtpClient()
+                {
+                    Port = 25,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = "your-host.com",
+                    EnableSsl = false,
+                    Credentials = credentials
+                };
+                client.Send(mail);
+                return "Email Sent Successfully!";
+            }
+            catch (System.Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+
+    }
+}
